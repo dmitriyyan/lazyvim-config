d27f80f391c481342814361832b94aa3a39c70fe
Dmitrii <dmitriy.ya@protonmail.com>
2025-07-30 00:04:57 +0200

    update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,11 +3,15 @@
 -- Add any additional options here
 
 local opt = vim.opt
-opt.relativenumber = false
+opt.relativenumber = true
 opt.wrap = true
 
 opt.spell = true
-opt.spelllang = { "ru", "en" }
-
+opt.spelllang = { "en", "ru" }
+opt.spelloptions = "camel"
+opt.spellfile = {
+  vim.fn.stdpath("config") .. "/spell/en.utf-8.add",
+  vim.fn.stdpath("config") .. "/spell/ru.utf-8.add",
+}
 -- formatters
 vim.g.lazyvim_prettier_needs_config = false
